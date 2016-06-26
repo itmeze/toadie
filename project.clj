07d1739cc548c7e3f2392917f42dcc0cee1599c0
itmeze <michal.zygula@gmail.com>
2016-06-26 22:34:45 +0200

    Bump 0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toadie "0.1.0-SNAPSHOT"
+(defproject toadie "0.2.0"
   :description "Turn postgresql database into document storage!!"
   :url "https://github.com/itmeze/toadie"
   :license {:name "Eclipse Public License"
